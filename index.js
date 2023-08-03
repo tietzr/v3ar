b33adc856d99edd399370087ee43457de609e0e3
Rafael Tietz <rafael.tietz@zukk.com.br>
2023-08-03 12:24:39 -0400

    update deployment

--- index.js
+++ index.js
@@ -30,12 +30,7 @@ app.set('view engine', 'ejs');
 app.use('/api/user', userRoutes);
 app.use('/api/book', bookRoutes);
 
-var environment = process.env.NODE_ENV || 'development';
-console.log(environment);
-const keys = { BACK_END_URL: "http://localhost:3000" }
-if (environment !== "development"){
-  keys.BACK_END_URL = process.env.BACK_END_URL;
-}
+const keys = { APPSETTING_BACK_END_URL: process.env.APPSETTING_BACK_END_URL} ;
 
 app.get('/', function(req, res) {
   res.render('pages/index', keys);
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_CONNECTION_STRING, {
+mongoose.connect(process.env.APPSETTING_DB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
